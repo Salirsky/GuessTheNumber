fb7445bb855dacba14816061a5a40b7ea7c889a6
Marina Belova <salirsky@mail.ru>
2022-04-03 22:36:13 +0300

    Add function guessTheNumber

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "Use strict";
+
+let guessTheNumber = function () {
+  let x = 66;
+  let userNumb = prompt("Угадай число от 1 до 100");
+
+  if (userNumb === null || userNumb === "") {
+    alert("Игра окончена");
+  } else if (userNumb == x) {
+    alert("Поздравляю, вы угадали!");
+  } else if (userNumb > x) {
+    alert("Загаданное число меньше, попробуйте снова");
+    return guessTheNumber();
+  } else if (userNumb < x) {
+    alert("Загаданное число больше, попробуйте снова");
+    return guessTheNumber();
+  } else if (!(!Number.isNaN(parseFloat(userNumb)) && isFinite(userNumb))) {
+    alert("Введите число");
+    return guessTheNumber();
+  } else {
+    alert("Хьюстон, у нас проблемы");
+  }
+};
+
+guessTheNumber();
